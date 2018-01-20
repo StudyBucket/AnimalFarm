e8b457a51db3a714495e0ca6fbc8bd47f277afc6
xYoshimax <Vmaliesk@smail.uni-koeln.de>
2018-01-20 23:37:53 +0100

    Neues/Altes Projekt

---
+++ PROJEKT/AnimalFarm/Assets/AimationTest/DoorController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour {
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if(collider.gameObject.tag == "Player1")
+        {
+            this.GetComponent<Animator>().SetTrigger("open");
+        }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player1")
+        {
+            this.GetComponent<Animator>().SetTrigger("close");
+        }
+    }
+
+}
