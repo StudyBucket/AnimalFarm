c81f8c81c4b847e92711a71f3cbdb412dba6de90
xYoshimax <Vmaliesk@smail.uni-koeln.de>
2018-01-23 19:17:38 +0100

    VR-Package und Animations-Scripts

---
+++ PROJEKT/AnimalFarm/Assets/Scripts/doorControllerClick.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorControllerClick : MonoBehaviour {
+
+    bool isOpen;
+
+    void OnMouseDown()
+    {
+        if (isOpen == false)
+        {
+            this.GetComponent<Animator>().SetTrigger("open");
+            isOpen = true;
+        } else
+        {
+            this.GetComponent<Animator>().SetTrigger("close");
+            isOpen = false;
+        }
+    }
+
+}
