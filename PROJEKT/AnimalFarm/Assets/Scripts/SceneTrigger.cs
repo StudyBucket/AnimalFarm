29071104150e86fb9d65f195ce35dad75fafe00e
Vera Malieske <Vmaliesk@smail.uni-koeln.de>
2018-05-14 03:49:30 +0200

    Animation Part 2
    - Szene Basis
    - Türanimationen fixed

---
+++ PROJEKT/AnimalFarm/Assets/Scripts/SceneTrigger.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class SceneTrigger : MonoBehaviour {
+
+    public PlayableDirector director;
+    public GameObject obj, player, timeline;
+    private bool looking;
+    public float minDistance = 10.0f;
+    private float distance;
+
+    // Use this for initialization
+    void Start () {
+        looking = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        distance = Vector3.Distance(player.transform.position, obj.transform.position);
+
+        if (looking)
+        {
+            if (distance <= minDistance)
+            {
+
+
+                if (Input.GetButtonDown("Fire1"))
+                {
+                    director = timeline.GetComponent<PlayableDirector>();
+
+                    if(director != null)
+                    {
+                        director.Play();
+                    }
+                }
+            }
+        }
+    }
+
+    #region IGvrGazeResponder implementation
+
+    public void OnGazeEnter()
+    {
+        looking = true;
+    }
+    public void OnGazeExit()
+    {
+        looking = false;
+    }
+    public void OnGazeTrigger()
+    {
+        looking = true;
+
+    }
+    #endregion
+}
---
+++ PROJEKT/AnimalFarm/Assets/Scripts/jumpingWell.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class jumpingWell : MonoBehaviour {
+
+    public PlayableDirector director;
+    public GameObject obj, player, well;
+    public bool jumpW;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            director = well.GetComponent<PlayableDirector>();
+            if (director != null)
+            {
+                director.Play();
+            }
+        }
+    }
+}
